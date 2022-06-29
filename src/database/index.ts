c937b7f984e2a4c66a6c99a30ad7e56ddf0c99f0
Gabriel <gabrielrad2@gmail.com>
2022-06-29 14:11:57 -0300

    Trabalhando com Banco de Dados

---
+++ src/database/index.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost", // for docker use "database" for local use "localhost" instead
+  port: 5432,
+  username: "docker",
+  password: "1234",
+  database: "rentalx",
+  synchronize: false,
+  logging: true,
+  migrations: ["./src/database/migration/*.ts"],
+  entities: ["./src/modules/**/entities/*.ts"],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch(error => console.log(error));
+
+export default AppDataSource;
